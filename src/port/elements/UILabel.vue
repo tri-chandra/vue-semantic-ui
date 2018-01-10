c01509e2dac9c7d3f6cd7dbca66e89e9b1ab114b
tri-chandra <tri-chandra@users.noreply.github.com>
2018-01-10 16:21:05 +1100

    Init label

--- src/port/elements/UILabel.vue
+++ src/port/elements/UILabel.vue
@@ -1,10 +1,82 @@
-<template>
-<div>
-</div>
-</template>
-
 <script>
-export default {
+import sizeMixin from '@/port/mixins/SizeMixin'
+import colorMixin from '@/port/mixins/ColorMixin'
 
+export default {
+  mixins: [sizeMixin, colorMixin],
+  props: {
+    image: Boolean,
+    tag: Boolean,
+    ribbon: Boolean,
+    attached: Boolean,
+    horizontal: Boolean,
+    floating: Boolean,
+    empty: Boolean,
+    basic: Boolean,
+
+    leftCorner: Boolean,
+    rightCorner: Boolean,
+
+    pointing: Boolean,
+    pointingBelow: Boolean,
+    leftPointing: Boolean,
+    rightPointing: Boolean,
+
+    link: Boolean,
+    detail: String
+  },
+  computed: {
+    classList() {
+      let retVal = ['ui', 'label']
+
+      for (let c in this.colorClass) {
+        retVal.splice(1, 0, c)
+      }
+      for (let c in this.sizeClass) {
+        retVal.splice(1, 0, c)
+      }
+
+      if (this.image) retVal.splice(1, 0, 'image')
+
+      if (this.pointing) retVal.splice(1, 0, 'pointing')
+      else if (this.pointingBelow) retVal.splice(1, 0, 'pointing below')
+      else if (this.leftPointing) retVal.splice(1, 0, 'left pointing')
+      else if (this.rightPointing) retVal.splice(1, 0, 'right pointing')
+
+      if (this.leftCorner) retVal.splice(1, 0, 'left corner')
+      if (this.rightCorner) retVal.splice(1, 0, 'right corner')
+
+      if (this.tag) retVal.splice(1, 0, 'tag')
+      if (this.ribbon) retVal.splice(1, 0, 'ribbon')
+      if (this.attached) retVal.splice(1, 0, 'attached')
+      if (this.horizontal) retVal.splice(1, 0, 'horizontal')
+      if (this.floating) retVal.splice(1, 0, 'floating')
+      if (this.empty) retVal.splice(1, 0, 'empty')
+      if (this.basic) retVal.splice(1, 0, 'basic')
+
+      return retVal
+    }
+  },
+  render(h) {
+    let children = []
+
+    if (this.detail) {
+      children.push(h(
+        'div',
+        { 'class' : 'detail' },
+        this.detail
+      ))
+    }
+
+    children.splice(0, 0, this.$slots.default)
+
+    return h(
+      this.link? 'a' : 'div',
+      {
+        'class': this.classList
+      },
+      children
+    )
+  }
 }
 </script>
--- src/wiki/elements/Label.vue
+++ src/wiki/elements/Label.vue
@@ -4,6 +4,125 @@
     Label
     <sui-header caption>A label displays content classification</sui-header>
   </sui-header>
+
+  <sui-header dividing h2>Types</sui-header>
+
+  <sui-header h4>Label</sui-header>
+  <p>A label</p>
+  <p>
+    <sui-label>
+      <sui-icon value="mail" /> 23
+    </sui-label>
+  </p>
+
+  <sui-header h4>Image</sui-header>
+  <p>A label can be formatted to emphasize an image</p>
+  <p>
+    <sui-label image>
+      <sui-image src="/static/img/jenny.jpg" /> Jenny
+    </sui-label>
+    <sui-label blue image detail="student">
+      <sui-image src="/static/img/jenny.jpg" /> Jenny
+    </sui-label>
+    <sui-label image>
+      <sui-image src="/static/img/jenny.jpg" />
+      Jenny
+      <sui-icon value="delete" />
+    </sui-label>
+  </p>
+
+  <sui-header h4>Pointing</sui-header>
+  <p>A label can point to content next to it</p>
+  <p>
+    <sui-label pointing>Please enter a value</sui-label>
+    <sui-label pointing-below>Please enter a value</sui-label>
+    <sui-label left-pointing>Please enter a value</sui-label>
+    <sui-label right-pointing>Please enter a value</sui-label>
+
+    <sui-label pointing red basic>Please enter a value</sui-label>
+    <sui-label pointing-below red basic>Please enter a value</sui-label>
+    <sui-label left-pointing red basic>Please enter a value</sui-label>
+    <sui-label right-pointing red basic>Please enter a value</sui-label>
+  </p>
+
+  <sui-header h4>Corner</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Tag</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Ribbon</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Attached</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Horizontal</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Pointing</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header dividing h2>Content</sui-header>
+
+  <sui-header h4>Detail</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Icon</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Image</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Link</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header dividing h2>Variations</sui-header>
+
+  <sui-header h4>Circular</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Basic</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Colored</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Size</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header dividing h2>Groups</sui-header>
+
+  <sui-header h4>Group Size</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Colored Group</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Tag Group</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
+  <sui-header h4>Circular Group</sui-header>
+  <p>Lorem ipsum</p>
+  <p></p>
+
 </div>
 </template>
 
