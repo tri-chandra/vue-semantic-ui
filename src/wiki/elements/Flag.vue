1456beb36c7117cc501fd75bfe606a22aca243a4
tri-chandra <tri-chandra@users.noreply.github.com>
2018-01-23 15:56:37 +1100

    Flag updated

--- src/wiki/elements/Flag.vue
+++ src/wiki/elements/Flag.vue
@@ -1,17 +1,52 @@
 <template>
-  <div>
-    <sui-header h1>
-      Flag
-      <sui-header caption>A flag is used to represent a political state</sui-header>
-    </sui-header>
-    <sui-flag value="ae" />
-    <sui-flag value="france" />
-    <sui-flag value="myanmar" />
-  </div>
+<wiki-template v-model="wikiContent"></wiki-template>
 </template>
 
 <script>
-  export default {
-
+export default {
+  data() {
+    return {
+      wikiContent: {
+        title: 'Flag',
+        caption: 'A flag is used to represent a political state',
+        sections: [
+          {
+            title: 'Definition',
+            subSections: [
+              {
+                title: 'Flag',
+                caption: 'A flag can use the two digit country code, the full name, or a common alias',
+                examples: [
+                  {
+                    code:
+`<div>
+  <sui-flag value="ae" />
+  <sui-flag value="france" />
+  <sui-flag value="myanmar" />
+</div>`
+                  }
+                ]
+              },
+              {
+                title: 'Types',
+                caption: '',
+                examples: [
+                  {
+                    note: 'Many flag sets use SVG to render flags at multiple sizes. However svg flags usually appear blurry or render improperly at smaller sizes. Semantic\'s default theme uses an image sprite solution which provides crisper icons, but without the ability to resize',
+                    code:
+`<div>
+  <sui-flag value="ad" />
+  <sui-flag value="bb" />
+  <sui-flag value="id" />
+</div>`
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
   }
+}
 </script>
